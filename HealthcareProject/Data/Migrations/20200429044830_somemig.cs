08390e1f79ba5ea28e319fd5dfd6b187785b9617
bniroula <44720521+bniroula@users.noreply.github.com>
2020-04-29 04:44:05 -0500

    Monthly and Daily Report

---
+++ HealthcareProject/Data/Migrations/20200429044830_somemig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HealthcareProject.Data.Migrations
+{
+    public partial class somemig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ HealthcareProject/Migrations/20200429070617_migrate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HealthcareProject.Migrations
+{
+    public partial class migrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "visitid",
+                table: "Visit_Record",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "visitid",
+                table: "Visit_Record");
+        }
+    }
+}
